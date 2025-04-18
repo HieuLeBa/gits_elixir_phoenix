b639cc270279dc75b476484bb99901c721ff97f3
HieuLB <hieudeu1xx1@gmail.com>
2025-04-18 17:17:15 +0700

    Video part 4: Afternoon 18042025

---
+++ lib/gits_elixir_phoenix/comments/comment.ex
@@ -0,0 +1,19 @@
+defmodule GitsElixirPhoenix.Comments.Comment do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "comments" do
+    field :markup_text, :string
+    belongs_to :user, GitsElixirPhoenix.Accounts.User
+    belongs_to :gist, GitsElixirPhoenix.Gists.Gist
+
+    timestamps(type: :utc_datetime)
+  end
+
+  @doc false
+  def changeset(comment, attrs) do
+    comment
+    |> cast(attrs, [:markup_text, :user_id, :gist_id])
+    |> validate_required([:markup_text, :user_id, :gist_id])
+  end
+end
